263f5f0a72ad2d276e872b276d33354420a1834b
KrisB <david.bierma@gmail.com>
2018-08-31 16:23:47 -0500

    setting up ProfileManager.js

---
+++ my-app/src/components/ProfileManager.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default class ProfileManager extends React.Component {
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+
+    }
+
+
+}
